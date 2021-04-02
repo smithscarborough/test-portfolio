09449d6b487eff02ee9117e88b25b03a0e150d5b
Smith Scarborough <smithscarborough@gmail.com>
2021-04-02 11:51:10 -0500

    attach twinkling stars container to home page background

--- portfolio-template/forms/contact.php
+++ portfolio-template/forms/contact.php
@@ -33,6 +33,7 @@
   );
   */
 
+    // PHP's version of POST requests
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
